1544a00da87985c8d6f349b18de43cb4a13eaf16
torukino <toruk0131@gmail.com>
2022-05-09 02:51:20 +0900

    2022050902

--- components/d3/CourceraD302.tsx
+++ components/d3/CourceraD302.tsx
@@ -11,78 +11,79 @@ interface DATA {
 }
 
 const CourceraD302 = ({ dataEvent }) => {
+  const ref = useRef(null)
   //--------- week 4 = Updating data  =====
   useEffect(() => {
     showData(dataEvent)
-  }, [])
+  }, [dataEvent])
 
-  const showData = (data) => {
+  //   const showData = (data) => {
+  // d3.csv('data.csv').then((data) => {
+  //   showData(data)
+  //   d3.select('#save').on('click', function () {
+  //     let name = d3.select('#name').node().value
+  //     let weight = d3.select('#weight').node().value
+  //     let client = data.find((d) => d.Name === name)
+  //     if (client) {
+  //       client.Weight = weight
+  //     }
+
+  //     showData(data)
+  //   })
+
+  //   d3.select('#min-weight').on('change', function () {
+  //     let value = this.value
+  //     const filteredData = data.filter((c) => +c.Weight > value)
+  //     showData(filteredData)
+  //   })
+  // })
+
+  const showData = (clients) => {
     d3.select('#container')
+    // d3.select(ref.current)
       .style('border', 'solid 1px #000')
       .style('margin-top', 10)
     d3.select('#body').style('transform', 'translate(40px,10px)')
 
     let body = d3.select('#body')
 
-    d3.csv('data.csv').then((data) => {
-      showData(data)
-      d3.select('#save').on('click', function () {
-        let name = d3.select('#name').node().value
-        let weight = d3.select('#weight').node().value
-        let client = data.find((d) => d.Name === name)
-        if (client) {
-          client.Weight = weight
-        }
+    let max = d3.max(clients, (d) => +d.Weight)
+    let scale = d3.scaleLinear().range([0, 60]).domain([0, max])
 
-        showData(data)
+    let scalePosition = d3
+      .scaleBand()
+      .rangeRound([0, 130])
+      .domain(clients.map((d) => d.Name))
+    scalePosition.padding(0.3)
+    let join = body.selectAll('rect').data(clients)
+
+    let newelements = join
+      .enter()
+      .append('rect')
+      .style('fill', 'blue')
+      .style('stroke', 'white')
+      .on('click', function (d) {
+        d3.select('#name').node().value = d.Name
+        d3.select('#weight').node().value = d.Weight
       })
 
-      d3.select('#min-weight').on('change', function () {
-        let value = this.value
-        const filteredData = data.filter((c) => +c.Weight > value)
-        showData(filteredData)
-      })
-    })
+    join
+      .merge(newelements)
+      .attr('width', (d) => scale(+d.Weight))
+      .attr('height', scalePosition.bandwidth())
+      .attr('transform', (d) => `translate(0,${scalePosition(d.Name)})`)
 
-    function showData(clients) {
-      let max = d3.max(clients, (d) => +d.Weight)
-      let scale = d3.scaleLinear().range([0, 60]).domain([0, max])
-
-      let scalePosition = d3
-        .scaleBand()
-        .rangeRound([0, 130])
-        .domain(clients.map((d) => d.Name))
-      scalePosition.padding(0.3)
-      let join = body.selectAll('rect').data(clients)
-
-      let newelements = join
-        .enter()
-        .append('rect')
-        .style('fill', 'blue')
-        .style('stroke', 'white')
-        .on('click', function (d) {
-          d3.select('#name').node().value = d.Name
-          d3.select('#weight').node().value = d.Weight
-        })
-
-      join
-        .merge(newelements)
-        .attr('width', (d) => scale(+d.Weight))
-        .attr('height', scalePosition.bandwidth())
-        .attr('transform', (d) => `translate(0,${scalePosition(d.Name)})`)
-
-      const yAxis = d3.axisLeft(scalePosition)
-      const yAxisContainer = d3
-        .select('#yAxis')
-        .style('transform', 'translate(40px, 10px)')
-        .transition()
-        .call(yAxis)
-    }
+    const yAxis = d3.axisLeft(scalePosition)
+    //   const yAxisContainer =
+    d3.select('#yAxis')
+      .style('transform', 'translate(40px, 10px)')
+      .transition()
+      .call(yAxis)
   }
 
   return (
     <>
-      <svg id="container" height="160" width="110">
+      <svg ref={ref} id="container" height="160" width="110">
         <g id="body"></g>
         <g id="yAxis"></g>
       </svg>
