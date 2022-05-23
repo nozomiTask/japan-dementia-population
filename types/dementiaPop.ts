9c912f5f054e161448d751bf5a28286b9a2067cd
torukino <toruk0131@gmail.com>
2022-05-24 03:03:05 +0900

    2022052402

--- types/dementiaPop.ts
+++ types/dementiaPop.ts
@@ -15,8 +15,26 @@ export interface DEMENTIAPOP {
   dRateMale65: number //65歳以上認知症有病率
   dRateFemale65: number
   dRateAll65: number
+
+/*
+  dPopMaleAge: agePop[] //x歳以上認知症人数
+  dPopFemaleAge: agePop[]
+  dPopAllAge: agePop[]
+  PopMaleAge: agePop[] //x歳以上人口
+  PopFemaleAge: agePop[]
+  PopAllAge: agePop[]
+  dRateMaleAge: agePop[] //x歳以上認知症有病率
+  dRateFemaleAge: agePop[]
+  dRateAllAge: agePop[]
+
+*/
   year: string
   order: number
   dementiaCategory: string //dementia, mci, dementiaAndMci
   source: string
+}
+
+interface agePop {
+  over: number
+  value: number
 }
