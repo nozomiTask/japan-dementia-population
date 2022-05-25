d8a1415bab79550838582053e0df00b4d8f9f320
torukino <toruk0131@gmail.com>
2022-05-26 01:20:38 +0900

    2022052601

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -4,7 +4,7 @@ export const Layout = (props) => {
   const { children, title = 'タイトル' } = props
 
   return (
-    <div className="flex flex-col min-h-screen bg-gray-100">
+    <div className="flex flex-col w-[900px] bg-gray-100">
       <header className="justify-around bg-gray-200 h-24 md:h-12 flex">
         <Link href="https://www.nozomi-mem.jp/">
           <a className="flex justify-items-start text-xl hover:opacity-30">
@@ -22,9 +22,10 @@ export const Layout = (props) => {
 
         <div className="items-center justify-between flex text-sm md:text-lg bg-color-gray-400">
           <Link href="https://www.nozomi-mem.jp/">
-             <a className="ml-6 text-xl text-mono text-green-500">のぞみメモリークリニックのホームページへ</a>
+            <a className="ml-6 text-xl text-mono text-green-500">
+              のぞみメモリークリニックのホームページへ
+            </a>
           </Link>
-         
         </div>
       </header>
       <main className="flex-grow ml-10 ">{children}</main>
