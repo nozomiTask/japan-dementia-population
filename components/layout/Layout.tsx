cdd8b169b9313e540999dcecffe6b694f18ea60e
Toru Kinoshita <toruk0131@gmail.com>
2022-05-25 12:48:24 +0900

    2022052502

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,25 +1,39 @@
 import Link from 'next/link'
-
+import Image from 'next/image'
 export const Layout = (props) => {
   const { children, title = 'タイトル' } = props
 
   return (
     <div className="flex flex-col min-h-screen bg-gray-100">
-      <header className="justify-around bg-gray-300 h-24 md:h-12 flex">
-        <div className="flex items-center justify-round">
-          <Link href="/dashboard/initPage">
-            <a className="text-xl hover:opacity-30">ホーム</a>
+      <header className="justify-around bg-gray-200 h-24 md:h-12 flex">
+        <Link href="https://www.nozomi-mem.jp/">
+          <a className="flex justify-items-start text-xl hover:opacity-30">
+            <Image
+              src="/nozomi.png"
+              alt="Nozomi_Memory_Clinic_Logo"
+              width={40}
+              height={40}
+            />
+            <span className="text-green-500 text-4xl">
+              のぞみメモリークリニック
+            </span>
+          </a>
+        </Link>
+
+        <div className="items-center justify-between flex text-sm md:text-lg bg-color-gray-400">
+          <Link href="https://www.nozomi-mem.jp/">
+             <a className="ml-6 text-xl text-mono text-green-500">のぞみメモリークリニックのホームページへ</a>
           </Link>
-        </div>
-        <div className="items-center justify-between flex text-sm md:text-lg">
-          <div className="ml-6 text-sm text-mono"></div>
+         
         </div>
       </header>
       <main className="flex-grow ml-10 ">{children}</main>
 
       <footer className="bg-gray-200">
         <p className="p-2 text-center text-xs">
-          Copyright © nozomi memory clinic, Apr. 2022. All Rights Reserved.
+          <a href="https://www.nozomi-mem.jp/">
+            Copyright © nozomi memory clinic, Apr. 2022. All Rights Reserved.
+          </a>
         </p>
       </footer>
     </div>
